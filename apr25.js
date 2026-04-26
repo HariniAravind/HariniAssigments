3960a1ff855a557529af0638504a7705d9c2a976
HariniAravind <harinivelu98@gmail.com>
2026-04-26 15:59:13 +0530

    Add files via upload

---
+++ apr25.js
@@ -0,0 +1,107 @@
+
+var firstName = 'Harini'
+console.log(firstName)
+console.log(typeof(firstName))
+var companyName = 'TCS'
+console.log(companyName)
+console.log(typeof(companyName))
+var mobileNumber = 9876543210
+console.log(mobileNumber)
+console.log(typeof(mobileNumber))
+var isAutomation = true
+console.log(isAutomation)
+console.log(typeof(isAutomation))
+var hasPlaywright=null
+console.log(hasPlaywright)
+console.log(typeof(hasPlaywright))
+
+
+// //Redeclaration using "var" keyword is allowed
+
+
+// var x // Memory Created and labelled the memory space as x
+
+
+// // Redeclaration 
+
+
+// var x  // Memory Created again and relabelled the memory space as x
+
+
+
+// console.log(x) // undefined
+
+
+
+// /* ************ */
+
+
+
+// //Reassignment using "var" keyword is allowed
+
+
+// var x =123 // Memory Created and labelled the memory space as x
+
+
+// // Redeclaration 
+
+
+// x=456  // Memory Created again and relabelled the memory space as x
+
+
+
+// console.log(x) // 456 // reassignment is allowed using var
+
+
+/* 
+In other pgm language redeclarion is not possible
+int x 
+int x */
+
+
+
+ //Redeclaration using "let" keyword is not allowed
+
+
+//let y// Memory Created and labelled the memory space as x
+ 
+
+
+//let y  // Memory Created again and relabelled the memory space as x
+
+
+//console.log(y); // Reference error
+
+
+
+ //Reassignment using "let" keyword is allowed
+
+
+// let sal=10000// Memory Created and labelled the memory space as x
+
+
+// // Reassignment is allowed 
+
+
+// sal=20000  // Memory Created again and relabelled the memory space as x
+
+
+// console.log(sal); // Reference error
+
+
+
+// //Declaration using "const" keyword 
+
+
+const z=898// Memory Created and labelled the memory space as x
+
+
+// Redeclaration using "const" is not allowed
+
+
+z=6765 // Memory Created again and relabelled the memory space as x
+
+
+console.log(z); // Reference error because we cannot assigne a new value to a varible like final value like your aadhar number
+
+
